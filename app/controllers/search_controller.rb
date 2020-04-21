211c75824cf5b76ba0c5a642c092a7c2829880ce
vagrant <vagrant@localhost.localdomain>
2020-04-21 12:06:23 +0000

    [add]topic9

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
     def search
         @search_target = params[:search_target]
-        if @search_target == 1
+        if @search_target == "1"
             @users = User.search(params[:search])
         else
             @books = Book.search(params[:search])
